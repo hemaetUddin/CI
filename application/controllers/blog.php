6a02a98dbf2cd3e29a6af577a9dc3b6233661c8c
Rinto Sarkar <rinto.sp@gmail.com>
2015-02-12 16:04:04 +0600

    second commit adding ci files

---
+++ application/controllers/blog.php
@@ -0,0 +1,20 @@
+<?php 
+	/**
+	* 
+	*/
+	class Blog extends CI_Controller
+	{
+		
+		function index()
+		{
+			echo "Rinto";
+		}
+
+		function dosomething(){
+			echo "Do something";
+		}
+	}
+
+
+
+?>
